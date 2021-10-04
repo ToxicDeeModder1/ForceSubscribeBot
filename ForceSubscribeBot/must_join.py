754cf4dfe14cc22dc7123a50768dc79d55900152
Toxic Dee Modder <90701056+ToxicDeeModder1@users.noreply.github.com>
2021-10-04 18:50:24 +0200

    Update must_join.py

--- ForceSubscribeBot/must_join.py
+++ ForceSubscribeBot/must_join.py
@@ -13,13 +13,13 @@ async def must_join_channel(bot: Client, msg: Message):
             await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
+                link = "https://t.me/TheBotsWorldChannel" + MUST_JOIN
             else:
                 chat_info = await bot.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"You must join [this channel]({link}) to use me. After joining try again !",
+                    f"You must join [this channel]({link}) to use me. After joining try again !!\n\n✋Made With L❤Ve By @ToxicDeeModderr!",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton("✨ Join Channel ✨", url=link)]
